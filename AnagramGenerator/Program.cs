ac26ae81434f87a5acaa9fce03edcf1b1af1a641
lbrooks81 <143453730+lbrooks81@users.noreply.github.com>
2024-11-29 11:08:22 -0500

    Add files via upload

---
+++ AnagramGenerator/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace AnagramGenerator
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            List<String> anagrams = AnagramGeneration("abcd");
+            foreach(String anagram in anagrams) 
+            {
+                Console.WriteLine(anagram);
+            }
+
+            Console.WriteLine($"\nThere are {anagrams.Count} anagrams.");
+        }
+
+        private static List<String> AnagramGeneration(String word)
+        {
+            // Base case, only one letter is left in the word
+            if (word.Length == 1)
+            {
+                return [word];
+            }
+
+            List<String> anagrams = [];
+
+            // Calls the method with the first character removed
+            List<String> subAnagrams = AnagramGeneration(word.Substring(1));
+
+            // Iterating over each substring anagram
+            foreach(String subAnagram in subAnagrams) 
+            {
+                for(int i = 0; i < word.Length; i++) 
+                {
+                    anagrams.Add(subAnagram.Insert(i, word[0].ToString()));
+                }
+            }
+
+            return anagrams;
+        }
+    }
+}
